df12d59439fa6fd723731090bc5c72ec4310e7a2
Levon_Aloyan <>
2023-03-30 13:06:03 +0400

    Initial commit

---
+++ src/main/java/com/epam/money_management/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.epam.money_management;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MoneyManagementApplication.class);
+    }
+
+}
