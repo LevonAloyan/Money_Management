a8838b000673b65c7a267b05d32af6cdacaecb96
Gohar Minasyan <goharminasyan80@gmail.com>
2023-04-03 17:19:10 +0400

    Created Debt and Admin

--- src/main/java/com/epam/money_management/rest/controller/AdminController.java
+++ src/main/java/com/epam/money_management/rest/controller/AdminController.java
@@ -1,7 +1,33 @@
 package com.epam.money_management.rest.controller;
 
+import com.epam.money_management.constants.Type;
+import com.epam.money_management.model.dto.DebtDto;
+import com.epam.money_management.rest.service.DebtService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/home/")
 public class AdminController {
+
+    private final DebtService debtService;
+
+    public AdminController(DebtService debtService) {
+        this.debtService = debtService;
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<List<DebtDto>> open(@PathVariable("id") Long adminId) {
+        debtService.allLenders(adminId, Type.LENT);
+        debtService.allLenders(adminId, Type.BORROWED);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+
 }
--- src/main/java/com/epam/money_management/rest/service/DebtService.java
+++ src/main/java/com/epam/money_management/rest/service/DebtService.java
@@ -1,4 +1,14 @@
 package com.epam.money_management.rest.service;
 
+import com.epam.money_management.constants.Type;
+import com.epam.money_management.model.dto.DebtDto;
+
+import java.util.List;
+
 public interface DebtService {
+
+    List<DebtDto> findAllByAdminId(Long adminId);
+
+    List<DebtDto> allLenders(Long adminId, Type type);
+
 }
