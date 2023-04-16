d475a966496dd080b5747a14a183b1c0893023a5
Gohar Minasyan <goharminasyan80@gmail.com>
2023-04-16 15:04:59 +0400

    changes

--- src/main/java/com/epam/money_management/rest/controller/DebtController.java
+++ src/main/java/com/epam/money_management/rest/controller/DebtController.java
@@ -1,6 +1,7 @@
 package com.epam.money_management.rest.controller;
 
 import com.epam.money_management.constants.Type;
+import com.epam.money_management.model.dto.DebtDto;
 import com.epam.money_management.rest.service.CreditorService;
 import com.epam.money_management.rest.service.DebtService;
 import org.springframework.stereotype.Controller;
@@ -9,34 +10,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/{adminId}/")
 public class DebtController {
     private final DebtService debtService;
     private final CreditorService creditorService;
+
     public DebtController(DebtService debtService, CreditorService creditorService) {
         this.debtService = debtService;
         this.creditorService = creditorService;
     }
 
-    @GetMapping("borrowed/{creditorId}")
+    @GetMapping("debt/{creditorId}")
     public String concreteCreditorBorrows(@PathVariable("adminId") Long adminId,
                                           @PathVariable("creditorId") Long creditorId, Model model) {
 
-        model.addAttribute("concreteCreditorBorrows",
-                debtService.findByCreditorIdAdminIdAndTypeOfDebt(creditorId, adminId, Type.BORROWED));
-        model.addAttribute("type", "BORROWED");
+        List<DebtDto> allDebtsOdConcreteCreditor = new ArrayList<>();
         model.addAttribute("creditor", creditorService.findById(creditorId).getFullName());
-        return "concreteBorrows";
-    }
-    @GetMapping("lent/{creditorId}")
-    public String concreteCreditorLents(@PathVariable("adminId") Long adminId,
-                              @PathVariable("creditorId") Long creditorId, Model model) {
-
-        model.addAttribute("concreteCreditorLents",
-                debtService.findByCreditorIdAdminIdAndTypeOfDebt(creditorId, adminId, Type.LENT));
-        model.addAttribute("type", "LENT");
-        model.addAttribute("creditor", creditorService.findById(creditorId).getFullName());
-        return "concreteLents";
+        allDebtsOdConcreteCreditor.addAll(debtService.findByCreditorIdAdminIdAndTypeOfDebt(creditorId, adminId, Type.LENT));
+        allDebtsOdConcreteCreditor.addAll(debtService.findByCreditorIdAdminIdAndTypeOfDebt(creditorId, adminId, Type.BORROWED));
+        model.addAttribute("allDebtsOdConcreteCreditor", allDebtsOdConcreteCreditor);
+        return "concreteDebts";
     }
 }
